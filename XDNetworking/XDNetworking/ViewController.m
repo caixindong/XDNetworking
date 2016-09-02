b4cd227c272f31f97d746f913b868844059c66f9
caixindong <458770054@qq.com>
2016-09-02 17:45:33 +0800

    clean

--- XDNetworking/XDNetworking/ViewController.m
+++ XDNetworking/XDNetworking/ViewController.m
@@ -19,9 +19,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-        NSLog(@"size is %lu",(unsigned long)[XDNetworking totalCacheSize]);
+    NSLog(@"size is %lu",(unsigned long)[XDNetworking totalCacheSize]);
     
-    NSLog(@"%@",[self md5:@"http://115.29.228.168:9191/workmap_dianAn/command/command.do?opCommand=userAction+1122"]);
+
 
     
     
@@ -35,9 +35,7 @@
                                                                                                                                      } progressBlock:^(int64_t bytesRead, int64_t totalBytes) {
                                                                                                                                          
                                                                                                                                      } successBlock:^(id response) {
-                                                                                                                                         //NSLog(@"response is %@",response[@"other"]);
                                                                                                                                          
-                                                                                                                                             NSLog(@"sss size is %lu",(unsigned long)[XDNetworking totalCacheSize]);
                                                                                                                                      } failBlock:^(NSError *error) {
                                                                                                                                          
                                                                                                                                      }];
@@ -45,23 +43,6 @@
 }
 
 
-- (NSString *)md5:(NSString *)string {
-    if (string == nil || string.length == 0) {
-        return nil;
-    }
-    
-    unsigned char digest[CC_MD5_DIGEST_LENGTH],i;
-    
-    CC_MD5([string UTF8String],(int)[string lengthOfBytesUsingEncoding:NSUTF8StringEncoding],digest);
-    
-    NSMutableString *ms = [NSMutableString string];
-    
-    for (i = 0; i < CC_MD5_DIGEST_LENGTH; i++) {
-        [ms appendFormat:@"%02x",(int)(digest[i])];
-    }
-    
-    return [ms copy];
-}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
